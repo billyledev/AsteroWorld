9d1def8a73ce0aa54c4eff4332312a993f3c482b
billy <francois.martin580@gmail.com>
2018-11-14 10:58:33 +0100

    Passage de la classe clavier en module

--- js/game.js
+++ js/game.js
@@ -1,3 +1,5 @@
+import { Keyboard } from './keyboard.js';
+
 //Fonction appelée à la fin du chargement du DOM
 window.addEventListener('load', () => {
     //On créé une nouvelle instance du jeu
@@ -32,113 +34,5 @@ class Game
         this.ctx.clearRect(0, 0, this.width, this.height);
 
         requestAnimationFrame(this.animate.bind(this));
-    }
-}
-
-//Gestion du clavier
-//https://hacks.mozilla.org/2017/03/internationalize-your-keyboard-controls/
-class Keyboard
-{
-    constructor()
-    {
-        //Touches utilisées dans le jeu
-        this.keys = {
-            up: false,
-            left: false,
-            down: false,
-            right: false
-        };
-
-        //Touche enfoncée
-        document.addEventListener('keydown', event => {
-            if (event.defaultPrevented)
-            {
-                return;
-            }
-
-            //Garde les racourcis clavier du navigateur
-            if (event.ctrlKey || event.altKey || event.metaKey || event.shiftKey)
-            {
-                return;
-            }
-
-            //Utilisation de code qui ne dépend pas de la disposition du clavier
-            switch (event.code)
-            {
-                case 'KeyW':
-                {
-                    this.keys.up = true;
-                    break;
-                }
-
-                case 'KeyA':
-                {
-                    this.keys.left = true;
-                    break;
-                }
-
-                case 'KeyS':
-                {
-                    this.keys.down = true;
-                    break;
-                }
-
-                case 'KeyD':
-                {
-                    this.keys.right = true;
-                    break;
-                }
-
-                default:
-                {
-                    return;
-                }
-            }
-
-            event.preventDefault();
-        });
-
-        //Touche relâchée
-        window.addEventListener('keyup', event => {
-            if (event.defaultPrevented)
-            {
-                return;
-            }
-
-            //Utilisation de code qui ne dépend pas de la disposition du clavier
-            switch (event.code)
-            {
-                case 'KeyW':
-                {
-                    this.keys.up = false;
-                    break;
-                }
-
-                case 'KeyA':
-                {
-                    this.keys.left = false;
-                    break;
-                }
-
-                case 'KeyS':
-                {
-                    this.keys.down = false;
-                    break;
-                }
-
-                case 'KeyD':
-                {
-                    this.keys.right = false;
-                    break;
-                }
-
-                default:
-                {
-                    return;
-                }
-            }
-
-            event.preventDefault();
-        });
     }
 }
---
+++ js/keyboard.js
@@ -0,0 +1,108 @@
+//Gestion du clavier
+//https://hacks.mozilla.org/2017/03/internationalize-your-keyboard-controls/
+
+export class Keyboard
+{
+    constructor()
+    {
+        //Touches utilisées dans le jeu
+        this.keys = {
+            up: false,
+            left: false,
+            down: false,
+            right: false
+        };
+
+        //Touche enfoncée
+        document.addEventListener('keydown', event => {
+            if (event.defaultPrevented)
+            {
+                return;
+            }
+
+            //Garde les racourcis clavier du navigateur
+            if (event.ctrlKey || event.altKey || event.metaKey || event.shiftKey)
+            {
+                return;
+            }
+
+            //Utilisation de code qui ne dépend pas de la disposition du clavier
+            switch (event.code)
+            {
+                case 'KeyW':
+                {
+                    this.keys.up = true;
+                    break;
+                }
+
+                case 'KeyA':
+                {
+                    this.keys.left = true;
+                    break;
+                }
+
+                case 'KeyS':
+                {
+                    this.keys.down = true;
+                    break;
+                }
+
+                case 'KeyD':
+                {
+                    this.keys.right = true;
+                    break;
+                }
+
+                default:
+                {
+                    return;
+                }
+            }
+
+            event.preventDefault();
+        });
+
+        //Touche relâchée
+        window.addEventListener('keyup', event => {
+            if (event.defaultPrevented)
+            {
+                return;
+            }
+
+            //Utilisation de code qui ne dépend pas de la disposition du clavier
+            switch (event.code)
+            {
+                case 'KeyW':
+                {
+                    this.keys.up = false;
+                    break;
+                }
+
+                case 'KeyA':
+                {
+                    this.keys.left = false;
+                    break;
+                }
+
+                case 'KeyS':
+                {
+                    this.keys.down = false;
+                    break;
+                }
+
+                case 'KeyD':
+                {
+                    this.keys.right = false;
+                    break;
+                }
+
+                default:
+                {
+                    return;
+                }
+            }
+
+            event.preventDefault();
+        });
+    }
+}
