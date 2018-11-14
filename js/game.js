a842166a037511cf78f32ce7fb1d2a855c782d33
Samuel Custodio Cavaco <scavaco@laposte.net>
2018-11-14 11:07:32 +0100

    Class Vaisseau et Tir + affichage Image

--- js/game.js
+++ js/game.js
@@ -1,5 +1,3 @@
-import { Keyboard } from './keyboard.js';
-
 //Fonction appelée à la fin du chargement du DOM
 window.addEventListener('load', () => {
     //On créé une nouvelle instance du jeu
@@ -24,7 +22,9 @@ class Game
         this.ctx = this.canvas.getContext('2d');
 
         this.keyboard = new Keyboard();
-
+        
+        this.vaisseau = new Vaisseau(100,100,this.ctx);
+        this.tir = new Tir(100,150,this.ctx);
         requestAnimationFrame(this.animate.bind(this));
     }
 
@@ -32,7 +32,149 @@ class Game
     animate()
     {
         this.ctx.clearRect(0, 0, this.width, this.height);
+        this.vaisseau.draw();
+        this.tir.draw();
         requestAnimationFrame(this.animate.bind(this));
-        //draw();
+        
+    }
+        
+}
+
+class Vaisseau{
+    constructor(x,y,ctx){
+        this.x=x;
+        this.y=y;
+        this.ctx=ctx;
+        this.img = new Image();
+        this.img.src = './js/Image/Vaisseau.png';
+    }
+    
+    draw(){
+        this.ctx.drawImage(this.img, this.x, this.y);
+    }
+    
+}
+
+class Tir{
+    constructor(x,y,ctx){
+        this.x=x;
+        this.y=y;
+        this.ctx=ctx;
+        this.img = new Image();
+        this.img.src = './js/Image/tir1.png';
+    }
+    
+    draw(){
+        this.ctx.drawImage(this.img, this.x, this.y);
+    }
+}
+
+
+//Gestion du clavier
+//https://hacks.mozilla.org/2017/03/internationalize-your-keyboard-controls/
+
+class Keyboard
+{
+    constructor()
+    {
+        //Touches utilisées dans le jeu
+        this.keys = {
+            up: false,
+            left: false,
+            down: false,
+            right: false
+        };
+
+        //Touche enfoncée
+        document.addEventListener('keydown', event => {
+            if (event.defaultPrevented)
+            {
+                return;
+            }
+
+            //Garde les racourcis clavier du navigateur
+            if (event.ctrlKey || event.altKey || event.metaKey || event.shiftKey)
+            {
+                return;
+            }
+
+            //Utilisation de code qui ne dépend pas de la disposition du clavier
+            switch (event.code)
+            {
+                case 'KeyW':
+                {
+                    this.keys.up = true;
+                    break;
+                }
+
+                case 'KeyA':
+                {
+                    this.keys.left = true;
+                    break;
+                }
+
+                case 'KeyS':
+                {
+                    this.keys.down = true;
+                    break;
+                }
+
+                case 'KeyD':
+                {
+                    this.keys.right = true;
+                    break;
+                }
+
+                default:
+                {
+                    return;
+                }
+            }
+
+            event.preventDefault();
+        });
+
+        //Touche relâchée
+        window.addEventListener('keyup', event => {
+            if (event.defaultPrevented)
+            {
+                return;
+            }
+
+            //Utilisation de code qui ne dépend pas de la disposition du clavier
+            switch (event.code)
+            {
+                case 'KeyW':
+                {
+                    this.keys.up = false;
+                    break;
+                }
+
+                case 'KeyA':
+                {
+                    this.keys.left = false;
+                    break;
+                }
+
+                case 'KeyS':
+                {
+                    this.keys.down = false;
+                    break;
+                }
+
+                case 'KeyD':
+                {
+                    this.keys.right = false;
+                    break;
+                }
+
+                default:
+                {
+                    return;
+                }
+            }
+
+            event.preventDefault();
+        });
     }
 }
